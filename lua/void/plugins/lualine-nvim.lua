e6752d11014fc436733ea1c48a84bd742c660c4c
balu401 <balubandgar0428@gmail.com>
2026-06-21 18:51:06 +0530

    Added Status Line for IDE

---
+++ lua/void/plugins/lualine-nvim.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+    options = {     
+      theme = "gruvbox", 
+      icons_enabled = true,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = {},
+        winbar = {},
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = true, -- Sets a signal status line at the very bottom of Neovim
+    },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" }
+    },
+  })
+end,
+}
